a351816ce4777bd851641f0175a8f7ec62e474ab
edwarhman <edwarhman@gmail.com>
2022-03-15 00:00:43 -0400

    TASK-3: add function to swap ETH

--- contracts/Swapper.sol
+++ contracts/Swapper.sol
@@ -2,13 +2,29 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
-import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router01.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 contract Swapper is Initializable {
-    ISwapRouter public swapRouter;
+    IUniswapV2Router01 public swapRouter;
 
-    function initialize(ISwapRouter _swapRouter) public {
+    function initialize(IUniswapV2Router01 _swapRouter) public {
         swapRouter = _swapRouter;
+    }
+
+    function swapETHForTokens(
+        uint256 amountIn,
+        uint amountOutMin,
+        address tokenAddress
+    ) external {
+        address[] memory path = new address[](2);
+        path[0] = tokenAddress;
+        path[1] = swapRouter.WETH();
+        swapRouter.swapExactETHForTokens(
+            amountOutMin,
+            path,
+            msg.sender,
+            block.timestamp
+        );
     }
 }
