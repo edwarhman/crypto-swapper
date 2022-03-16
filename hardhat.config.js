48b85d12091ba71533de6a19a686e99bc967dc74
edwarhman <edwarhman@gmail.com>
2022-03-16 14:06:59 -0400

    TASK-23: add solidity coverage plugin to project

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 require('@openzeppelin/hardhat-upgrades');
 require("dotenv").config();
+require("solidity-coverage");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
